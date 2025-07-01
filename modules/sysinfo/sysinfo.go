9a3a6e397e76b85daf0351c2f9988e45dbf34eca
claudedigon <claudedigon@gmail.com>
2025-07-02 00:47:28 +0300

    tech: added the 'sysinfo' module for host OS and hardware info collection

---
+++ modules/sysinfo/sysinfo.go
@@ -0,0 +1,57 @@
+package sysinfo
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/hypernetix/hyperspot/libs/core"
+	"github.com/hypernetix/hyperspot/libs/db"
+	"github.com/hypernetix/hyperspot/libs/errorx"
+	"github.com/hypernetix/hyperspot/libs/utils"
+	"gorm.io/gorm"
+)
+
+func GetSysInfo() (*utils.SystemInfo, error) {
+	return utils.CollectSysInfo()
+}
+
+func GetSysInfoPersisted() (*utils.SystemInfo, errorx.Error) {
+	sysInfo, err := utils.CollectSysInfo()
+	if err != nil {
+		return nil, errorx.NewErrInternalServerError("failed to collect sysinfo: %s", err.Error())
+	}
+	err = SaveSysInfoToDB(sysInfo)
+	if err != nil {
+		return nil, errorx.NewErrInternalServerError("failed to save sysinfo to db: %s", err.Error())
+	}
+	return sysInfo, nil
+}
+
+func GetSysInfoFromDB(id uuid.UUID) (*utils.SystemInfo, errorx.Error) {
+	var sysInfo utils.SystemInfo
+	if err := db.DB.Where("id = ?", id).First(&sysInfo).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, errorx.NewErrNotFound("sysinfo record not found")
+		}
+		return nil, errorx.NewErrInternalServerError("failed to get sysinfo from db: %s", err.Error())
+	}
+	return &sysInfo, nil
+}
+
+func SaveSysInfoToDB(sysInfo *utils.SystemInfo) error {
+	_, errx := GetSysInfoFromDB(sysInfo.ID)
+	if errx == nil {
+		return nil
+	}
+	return db.DB.Save(sysInfo).Error
+}
+
+func InitModule() {
+	core.RegisterModule(&core.Module{
+		Migrations: []interface{}{
+			&utils.SystemInfo{},
+		},
+		InitAPIRoutes: initSysInfoAPIRoutes,
+		Name:          "sysinfo",
+	})
+}
---
+++ modules/sysinfo/sysinfo_api.go
@@ -0,0 +1,63 @@
+package sysinfo
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/google/uuid"
+	"github.com/hypernetix/hyperspot/libs/api"
+	"github.com/hypernetix/hyperspot/libs/errorx"
+	"github.com/hypernetix/hyperspot/libs/utils"
+)
+
+type SysInfoAPIResponse struct {
+	Body utils.SystemInfo `json:"body"`
+}
+
+type GetSysInfoByIDInput struct {
+	ID string `path:"id" doc:"System info ID"`
+}
+
+func initSysInfoAPIRoutes(humaApi huma.API) {
+	// GET /sysinfo - Get current system information
+	api.RegisterEndpoint(humaApi, huma.Operation{
+		OperationID: "get-sysinfo",
+		Method:      http.MethodGet,
+		Path:        "/sysinfo",
+		Summary:     "Get current system information",
+		Tags:        []string{"System Information"},
+	}, func(ctx context.Context, input *struct{}) (*SysInfoAPIResponse, error) {
+		sysInfo, err := GetSysInfo()
+		if err != nil {
+			return nil, errorx.NewErrInternalServerError(err.Error())
+		}
+
+		return &SysInfoAPIResponse{
+			Body: *sysInfo,
+		}, nil
+	})
+
+	// GET /sysinfo/{id} - Get historical system information by ID
+	api.RegisterEndpoint(humaApi, huma.Operation{
+		OperationID: "get-sysinfo-by-id",
+		Method:      http.MethodGet,
+		Path:        "/sysinfo/{id}",
+		Summary:     "Get historical system information by ID",
+		Tags:        []string{"System Information"},
+	}, func(ctx context.Context, input *GetSysInfoByIDInput) (*SysInfoAPIResponse, error) {
+		sysInfoId, err := uuid.Parse(input.ID)
+		if err != nil {
+			return nil, huma.Error400BadRequest("Invalid sysinfo ID")
+		}
+
+		sysInfo, errx := GetSysInfoFromDB(sysInfoId)
+		if errx != nil {
+			return nil, errx.HumaError()
+		}
+
+		return &SysInfoAPIResponse{
+			Body: *sysInfo,
+		}, nil
+	})
+}
